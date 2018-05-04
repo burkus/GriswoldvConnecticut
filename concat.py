8d1d18298287d47a34eb8751b7d46778755c3141
Andrew G Burkus <da8urk@gmail.com>
2018-05-04 12:31:45 -0500

    initial commit

---
+++ concat.py
@@ -0,0 +1,30 @@
+#! python
+# Concatenate the slides into a single file
+import os
+from os.path import isfile
+
+slidesPath = os.getcwd() + r"\slides"
+slideFiles = os.listdir(slidesPath)
+
+slides = ""
+for file in slideFiles:
+    filePath = slidesPath + "\\" + file
+    html = open(filePath, 'r').read()
+    slides += html
+
+def writeConcatFile(contents):
+    concatFilePath = os.getcwd() + r"\concat.html"
+    if isfile(concatFilePath):
+        os.remove(concatFilePath)
+    concatFile = open('concat.html', 'w+')
+    concatFile.write(contents)
+
+templateFilePath = os.getcwd() + r"\template.html"
+templateFileHtml = open(templateFilePath, "r").read()
+finalString = ""
+if "$" in templateFileHtml:
+    insertIndex = templateFileHtml.find('$')
+    preInsertString = templateFileHtml[0:insertIndex]
+    postInsertString = templateFileHtml[insertIndex + 1:]
+    finalString = preInsertString + slides + postInsertString
+    writeConcatFile(finalString)
